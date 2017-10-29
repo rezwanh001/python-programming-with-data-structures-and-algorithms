1987120038bafacfad44389df8e6d5477cf1090b
Rezwan <rezwan@Rezwans-iMac.local>
2017-10-29 19:51:16 +0600

    1. Introduction

---
+++ Hello Python.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+print('Welcome to Python World!') # print this sentence with new line(default)
+
+print('Python World! without new line', end='') # print this sentence without new-line.
+
+print() # pyhton simple new line.
+
+print("Hello wold!", "Hello Dunia!", "Hello Universe!") # Three sentence with one-space (default).
+
+print("\n") # this means 2 new-line
