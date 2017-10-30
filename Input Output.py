45dfcc24acab9774cbfca1a5e69a7259e04835e7
Rezwan <rezwan@Rezwans-iMac.local>
2017-10-30 14:18:05 +0600

    1. Introduction

--- Input Output.py
+++ Input Output.py
@@ -1,28 +1,94 @@
-a = input() # take a string (with-space, without-space)
-# hello world
-print(a) # hello world
-
-
-a = int(input()) # take only one int number
-# 5
-print(a) # 5
-
-a = float(input()) # take only one float number
+######### take a string (with-space, without-space)
+a = input()
 print(a)
+'''
+Input:
+Hello World!
 
-a = str(input()) # take only one str
+Output:
+Hello World!
+'''
+
+
+######### take only one int number
+a = int(input())
 print(a)
+'''
+Input:
+5
 
-a, b = map(int, input().split()) # take two numbers with only one space. like : 2 3
-print(a, b) # 2 3
+Output:
+5
+'''
 
-a, b, c = map(int, input().split()) # take three numbers with only one space # 2 3 4
-print(a, b, c) # 2 3 4
 
-# input a list
+######### take only one float number
+a = float(input())
+print(a)
+'''
+Input:
+5.6
+
+Output:
+5.6
+'''
+
+##### take only one str
+a = str(input())
+print(a)
+'''
+Input:
+Rezwan
+
+Output:
+Rezwan
+'''
+
+
+
+
+###### take two numbers with only one space. like : 2 3
+a, b = map(int, input().split())
+print(a, b)
+'''
+Input:
+2 3
+
+Output:
+2 3
+'''
+
+
+
+
+######### take three numbers with only one space
+a, b, c = map(int, input().split())
+print(a, b, c)
+'''
+Input:
+2 3 4
+
+Output:
+2 3 4
+'''
+
+
+
+############### input a list
 arr = list(map(int, input().split())) # int type array
-print(arr) # [1, 2, 3, 4, 5]
-print(*arr) # 1 2 3 4 5
+print(arr)
+print(*arr)
+'''
+Input:
+1 2 3 4 5 
+
+Output:
+[1, 2, 3, 4, 5]
+1 2 3 4 5
+
+'''
+
+#########################
 
 a = int(input("Enter a Integer number: "))
 print("Entered number is %d" % a)
@@ -30,6 +96,8 @@ print("Entered number is %d" % a)
 Enter a Integer number: 233
 Entered number is 233
 '''
+
+##########################
 
 a = float(input("Enter a Float number: "))
 print("Entered number is %f" % a)
---
+++ String Formatting.py
@@ -0,0 +1,47 @@
+######## String length, uppercase, lowercase
+a = input("Enter a string with uppercase and lowercase: ")
+print(a.upper())
+print(a.lower())
+print(len(a))
+'''
+Input:
+Hello World!
+
+Output:
+HELLO WORLD!
+hello world!
+12
+'''
+
+##### Separate string with a character
+print("Welcome", "to", "python", "programming", sep="*")
+'''
+Output:
+Welcome*to*python*programming
+'''
+
+################################
+name = input()
+age = int(input())
+print(name, "is" , age, "years old!")
+print('%s is %d years old!' %(name, age))
+'''
+Input:
+Rezwan
+20
+
+Output:
+Rezwan is 20 years old!
+Rezwan is 20 years old!
+'''
+
+##################################
+a = float(input())
+print("Price of this t-shirt is %.10f" % a)
+'''
+Input:
+855.75
+
+Output:
+Price of this t-shirt is 855.7500000000
+'''
